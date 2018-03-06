faf72b074bd4298ad65bcd1481e21c485ee2ce39
Chase <chaseewhitney@gmail.com>
2018-03-06 15:07:10 -0600

    server pulls data from website then filters and formats it

--- server/server.js
+++ server/server.js
@@ -6,9 +6,6 @@ var request = require('request');
 var port = 5001;
 var cheerio = require('cheerio');
 
-//var ingredientList = [];
-//var url = process.env.URL || require('../config.js').url;
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -17,6 +14,21 @@ app.use(express.static('./server/public'));
 app.get('/*', function(req, res) {
   var file = req.params[0] || '/views/index.html';
   res.sendFile(path.join(__dirname, '/public/', file));
+});
+
+var ingredientList = [];
+var url = process.env.URL || require('../config.js').url;
+
+// Pull data from website and create an array of objects with name and rating of ingredients
+request(url, function(err, resp, html) {
+  if (!err){
+    const $ = cheerio.load(html);
+    $('.ingredient-result').each(function(index, element){
+      ingredientList[index] = {};
+      ingredientList[index]['name'] = $(element).find('.ingredient-info .ingredient-name a').text();
+      ingredientList[index]['rating'] = $(element).find('.ingredient-rating').text();
+    });
+  }
 });
 
 app.listen(port, function(){
