790180e40d19c339b7a2f3e8cf1a465b57c10763
Chase <chaseewhitney@gmail.com>
2018-03-06 19:00:08 -0600

    filter working with test data

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,12 +5,36 @@ myApp.controller('MyController', function($http, $scope) {
 
   $scope.test = "blue";
   $scope.ingData = [];
+  $scope.tempListString = "naPCa, myrrh, polyhydroxy acid, testfail extract, red raspberry extract, Saccharomyces lysate";
+  $scope.tempList = [];
+  $scope.ingRatings = [];
 
-  
+  // GETs array of ingredients and ratings from server
   $http.get('/getIngredientList').then(function(response){
     console.log('GET response data:', response.data);
     $scope.ingData = response.data;
     console.log('ingData:', $scope.ingData);
+    $scope.tempList = $scope.tempListString.split(", ");
+    console.log("tempList is:", $scope.tempList);
+
+    for (var i = 0; i < $scope.tempList.length; i++) {
+      var result = $scope.ingData.filter(function( obj ) {
+        return obj.name.toLowerCase() == $scope.tempList[i].toLowerCase();
+      });
+      if (result[0]) {
+        $scope.ingRatings.push(result[0]);
+      } else {
+        $scope.ingRatings.push({'name': $scope.tempList[i], 'rating':'Unknown'})
+      }
+
+    }
+
+    console.log('ingRatings:', $scope.ingRatings);
+
   });
+
+
+
+
 
 });
