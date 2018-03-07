a4bacd3e29bf6123cb564e8b587393c64ee41bec
Chase <chaseewhitney@gmail.com>
2018-03-06 18:22:05 -0600

    fixed README typo

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,9 +4,13 @@ var myApp = angular.module('myApp', []);
 myApp.controller('MyController', function($http, $scope) {
 
   $scope.test = "blue";
+  $scope.ingData = [];
 
+  
   $http.get('/getIngredientList').then(function(response){
     console.log('GET response data:', response.data);
+    $scope.ingData = response.data;
+    console.log('ingData:', $scope.ingData);
   });
 
 });
