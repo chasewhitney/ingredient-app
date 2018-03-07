c41aa6d6c60e39b9f14c65a7a00d38788ae645c3
Chase <chaseewhitney@gmail.com>
2018-03-07 15:03:22 -0600

    small formatting fixes

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -16,34 +16,24 @@ myApp.controller('MyController', function($http, $scope) {
     console.log('ingData:', $scope.ingData);
   });
 
-
-$scope.searchList = function(userIngredientList){
-  console.log('in searchList');
-
-  $scope.tempList = userIngredientList.split(", ");
-  console.log("tempList is:", $scope.tempList);
-
-  $scope.ingRatings = [];
-  for (var i = 0; i < $scope.tempList.length; i++) {
-    var result = $scope.ingData.filter(function( obj ) {
-      return obj.name.toLowerCase() == $scope.tempList[i].toLowerCase();
-    });
-    if (result[0]) {
-      $scope.ingRatings.push(result[0]);
-    } else {
-      $scope.ingRatings.push({'name': $scope.tempList[i], 'rating':'Unknown'})
+  // Searches known ingredients list and returns matches with name and rating
+  // If match isn't found, rating property becomes 'unknown'
+  $scope.searchList = function(userIngredientList){
+    console.log('in searchList');
+    $scope.tempList = userIngredientList.split(", ");
+    console.log("tempList is:", $scope.tempList);
+    $scope.ingRatings = [];
+    for (var i = 0; i < $scope.tempList.length; i++) {
+      var result = $scope.ingData.filter(function( obj ) {
+        return obj.name.toLowerCase() == $scope.tempList[i].toLowerCase();
+      });
+      if (result[0]) {
+        $scope.ingRatings.push(result[0]);
+      } else {
+        $scope.ingRatings.push({'name': $scope.tempList[i], 'rating':'Unknown'})
+      }
     }
-
-  }
-
-  console.log('ingRatings:', $scope.ingRatings);
-
-};
-
-
-
-
-
-
+    console.log('ingRatings:', $scope.ingRatings);
+  };
 
 });
