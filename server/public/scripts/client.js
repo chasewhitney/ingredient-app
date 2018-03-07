180750baa6d2e459dee7d5ae58c04efad396e61d
Chase <chaseewhitney@gmail.com>
2018-03-07 15:10:52 -0600

    changed some variable names, removed some test variables

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,37 +3,35 @@ var myApp = angular.module('myApp', ['ngMaterial', 'ngMessages']);
 
 myApp.controller('MyController', function($http, $scope) {
 
-  $scope.test = "blue";
-  $scope.ingData = [];
-  $scope.tempListString = "naPCa, myrrh, polyhydroxy acid, testfail extract, red raspberry extract, Saccharomyces lysate";
-  $scope.tempList = [];
-  $scope.ingRatings = [];
+  $scope.ingredientDictionary = [];
+  $scope.listToCheck = [];
+  $scope.ingredientRatings = [];
 
   // GETs array of ingredients and ratings from server
   $http.get('/getIngredientList').then(function(response){
     console.log('GET response data:', response.data);
-    $scope.ingData = response.data;
-    console.log('ingData:', $scope.ingData);
+    $scope.ingredientDictionary = response.data;
+    console.log('ingredientDictionary:', $scope.ingredientDictionary);
   });
 
   // Searches known ingredients list and returns matches with name and rating
   // If match isn't found, rating property becomes 'unknown'
   $scope.searchList = function(userIngredientList){
     console.log('in searchList');
-    $scope.tempList = userIngredientList.split(", ");
-    console.log("tempList is:", $scope.tempList);
-    $scope.ingRatings = [];
-    for (var i = 0; i < $scope.tempList.length; i++) {
-      var result = $scope.ingData.filter(function( obj ) {
-        return obj.name.toLowerCase() == $scope.tempList[i].toLowerCase();
+    $scope.listToCheck = userIngredientList.split(", ");
+    console.log("listToCheck is:", $scope.listToCheck);
+    $scope.ingredientRatings = [];
+    for (var i = 0; i < $scope.listToCheck.length; i++) {
+      var result = $scope.ingredientDictionary.filter(function( obj ) {
+        return obj.name.toLowerCase() == $scope.listToCheck[i].toLowerCase();
       });
       if (result[0]) {
-        $scope.ingRatings.push(result[0]);
+        $scope.ingredientRatings.push(result[0]);
       } else {
-        $scope.ingRatings.push({'name': $scope.tempList[i], 'rating':'Unknown'})
+        $scope.ingredientRatings.push({'name': $scope.listToCheck[i], 'rating':'Unknown'})
       }
     }
-    console.log('ingRatings:', $scope.ingRatings);
+    console.log('ingredientRatings:', $scope.ingredientRatings);
   };
 
 });
